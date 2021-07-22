d8995557f2df1873f8706961425846d12972425d
MaxLeduc <leduc.maximep@gmail.com>
2021-07-22 18:18:10 -0400

    footer (#7)
    * footer
    
    * polish
    '

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import { Provider } from 'urql';
 import Home from './core/Home'
 import {graphqlClient} from './common/api/graphqlClient'
 import {PlayersProvider} from './common/data/PlayersProvider'
+import { Footer } from './common/components/Footer';
 
 function App() {
   return (
     <Provider value={graphqlClient}>
       <PlayersProvider>
         <Home />
+        <Footer />
       </PlayersProvider>
     </Provider>
   );
