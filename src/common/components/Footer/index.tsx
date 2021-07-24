ea0636ac1bb3af037b786988713235e3e2edc838
MaxLeduc <leduc.maximep@gmail.com>
2021-07-24 17:55:39 -0400

    misc (#12)

--- src/common/components/Footer/index.tsx
+++ src/common/components/Footer/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
-import { Wrapper } from './styles'
+import { ExternalWrapper, SocialWrapper, TwitterLink, InnerWrapper } from './styles'
+import TwitterIcon from './assets/twitter.png'
 
 export const Footer = () => {
   const year = new Date().getFullYear()
 
-  return <Wrapper>
-    <p>&#169; {year} Maxime Leduc</p>
-  </Wrapper>
+  return <ExternalWrapper>
+    <InnerWrapper>
+      <p>&#169; {year} Maxime Leduc</p>
+      <SocialWrapper>
+        <p>Find me on Twitter:</p>
+        <TwitterLink rel="noopener noreferrer" target="_blank" href="https://twitter.com/ledukeness">
+          <img src={TwitterIcon} alt="twitter icon" />
+        </TwitterLink>
+      </SocialWrapper>
+    </InnerWrapper>
+  </ExternalWrapper>
 }
--- src/common/components/Footer/styles.ts
+++ src/common/components/Footer/styles.ts
@@ -1,8 +1,31 @@
 import styled from 'styled-components'
 
-export const Wrapper = styled.div`
-  padding: 15px;
-  text-align: center;
-  background: #EEEEEE;
+export const ExternalWrapper = styled.div`
+  padding: 14px;
+  background: #3f51b5;
   font-size: 13px;
+`
+  
+export const InnerWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: white;
+  width: 90%;
+  margin: 0 auto;
+`
+
+export const SocialWrapper = styled.div`
+  display: flex;
+  align-items: center;
+
+  > p {
+    margin-right: 10px;
+  }
+`
+
+export const TwitterLink = styled.a`
+ > img {
+   width: 20px;
+ }
 `
